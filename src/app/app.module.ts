73cdf3f2d2124f686503271dfae6ca81a59f1dec
jorgernes <sosapcsmx@gmail.com>
2018-08-02 15:05:02 -0500

    dff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ContainerComponent } from './container/container.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ContainerComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
